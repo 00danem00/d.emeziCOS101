5aef9ea2f4bc9133efd9fa8996b20bd5f322906d
00danem00 <daniel.emezi@pau.edu.ng>
2025-10-15 15:59:27 +0100

    week 3 practice 6 committed

---
+++ week-3/pactice_6/target/debug/deps/pactice_6.d
@@ -0,0 +1,5 @@
+C:\Users\HomePC\Documents\d.emeziCOS101\week-3\pactice_6\target\debug\deps\pactice_6.d: src\main.rs
+
+C:\Users\HomePC\Documents\d.emeziCOS101\week-3\pactice_6\target\debug\deps\pactice_6.exe: src\main.rs
+
+src\main.rs:
---
+++ week-3/pactice_6/target/debug/pactice_6.d
@@ -0,0 +1,1 @@
+C:\Users\HomePC\Documents\d.emeziCOS101\week-3\pactice_6\target\debug\pactice_6.exe: C:\Users\HomePC\Documents\d.emeziCOS101\week-3\pactice_6\src\main.rs
