43c709dcd86f4eaa5f085e5c9887ae2f3b4aab24
00danem00 <daniel.emezi@pau.edu.ng>
2025-10-22 13:21:12 +0100

    week 4 practice 3 commit

---
+++ week-4/practice_3/target/debug/deps/pracice_3.d
@@ -0,0 +1,5 @@
+C:\Users\HomePC\Documents\d.emeziCOS101\week-4\practice_3\target\debug\deps\pracice_3.d: src\main.rs
+
+C:\Users\HomePC\Documents\d.emeziCOS101\week-4\practice_3\target\debug\deps\pracice_3.exe: src\main.rs
+
+src\main.rs:
---
+++ week-4/practice_3/target/debug/pracice_3.d
@@ -0,0 +1,1 @@
+C:\Users\HomePC\Documents\d.emeziCOS101\week-4\practice_3\target\debug\pracice_3.exe: C:\Users\HomePC\Documents\d.emeziCOS101\week-4\practice_3\src\main.rs
