6c4782960232295adc65863e2ac08d2f648f2bce
00danem00 <daniel.emezi@pau.edu.ng>
2025-10-22 13:42:42 +0100

    week 4 prac 8B commit

---
+++ week-4/practice_8B/target/debug/deps/practice_8B.d
@@ -0,0 +1,5 @@
+C:\Users\HomePC\Documents\d.emeziCOS101\week-4\practice_8B\target\debug\deps\practice_8B.d: src\main.rs
+
+C:\Users\HomePC\Documents\d.emeziCOS101\week-4\practice_8B\target\debug\deps\practice_8B.exe: src\main.rs
+
+src\main.rs:
---
+++ week-4/practice_8B/target/debug/practice_8B.d
@@ -0,0 +1,1 @@
+C:\Users\HomePC\Documents\d.emeziCOS101\week-4\practice_8B\target\debug\practice_8B.exe: C:\Users\HomePC\Documents\d.emeziCOS101\week-4\practice_8B\src\main.rs
