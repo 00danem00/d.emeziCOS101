bba073e79ce623a236c7a36049c758d8c9e1305d
00danem00 <daniel.emezi@pau.edu.ng>
2025-11-05 20:14:29 +0100

    practice 11

---
+++ week-6/practice_11/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let a: i32 = 2;    // Bit presentation 10
+    let b: i32 = 3;    // Bit presentation 11
+
+    let mut result: i32;
+
+    result = a & b;
+    println!("a & b = {}", result);
+
+    result = a | b;
+    println!("a | b = {}", result);
+
+    result = a ^ b;
+    println!("a ^ b = {}", result);
+
+    result = a << 1;
+    println!("a << 1 = {}", result);
+
+    result = a >> 1;
+    println!("a >> 1 = {}", result);
+}
