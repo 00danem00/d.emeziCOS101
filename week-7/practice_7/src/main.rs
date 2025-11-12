ab9d0e273f2da14890eaba130f8c1d0efc10a86c
00danem00 <daniel.emezi@pau.edu.ng>
2025-11-12 13:52:29 +0100

    wk7 practice 7 commit

--- week-7/practice_7/src/main.rs
+++ week-7/practice_7/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    // Array with data type (explicit integer datatype)
+    let arr1: [i32; 4] = [10, 20, 30, 40];
+    println!("array is: {:?}", arr1);
+    println!("array size is: {}", arr1.len());
+
+    // Array without data type (implicit float datatype)
+    let arr2 = [10.4, 20.7, 30.4, 51.2, 72.3];
+    println!("array is: {:?}", arr2);
+    println!("array size is: {}", arr2.len());
+
+    // Array with default values that creates and initializes its elements with a default value of -1.
+    let arr3: [i32; 8] = [-1; 8];
+    println!("array with default values: {:?}", arr3);
+    println!("array size is: {}", arr3.len());
 }
---
+++ week-7/practice_7/target/debug/practice_7.d
@@ -0,0 +1,1 @@
+C:\Users\HomePC\Documents\d.emeziCOS101\week-7\practice_7\target\debug\practice_7.exe: C:\Users\HomePC\Documents\d.emeziCOS101\week-7\practice_7\src\main.rs
