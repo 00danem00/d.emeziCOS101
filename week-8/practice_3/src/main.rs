185abd54781fc987b83ba5e1ace937e800cb823f
00danem00 <daniel.emezi@pau.edu.ng>
2025-11-19 21:10:51 +0100

    i accessed a vector using the get method

---
+++ week-8/practice_3/src/main.rs
@@ -0,0 +1,22 @@
+ use std::io;
+
+ // methos to print the get value
+ fn value(n:Option<&char>)
+{
+    println!("Element of vector is {:?}",n);
+}
+
+fn main() {
+    let v = vec!['r','u','s','t','a','c','i','a','n'];
+
+    let mut input1 = String::new();
+    println!("\nEnter an index value (0-8)");
+    io::stdin().read_line(&mut input1).expect("failed to read input");
+
+    //index is the non negative value which is smaller than the size of the vector
+    let index:usize = input1.trim().parse().expect("invalid input");
+
+    //getting value at iven index value
+    let ch: Option<&char> = v.get(index);
+    value(ch);
+}
