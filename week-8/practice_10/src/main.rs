e7d4c16e6c150f0e64df08df0d5a6cabf4d813de
00danem00 <daniel.emezi@pau.edu.ng>
2025-11-21 23:28:23 +0100

    i destructured a tuple

---
+++ week-8/practice_10/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+
+    let b: (i32, bool,f64) = (30,true, 4.9);
+    print(b);
+}
+
+fn print(x:(i32,bool,f64)) {
+
+    println!("\nInside print method");
+    //assigns a tuple to distinct variables
+    let (age, is_male,cgpa) = x;
+    println!("Age is: {}, isMale?: {}, cgpa is: {}\n",age,is_male,cgpa);
+
+}
