d239e7d67e09890eff8b9042cfc283436c1df400
00danem00 <daniel.emezi@pau.edu.ng>
2025-11-23 23:13:26 +0100

    i created a rust program to find the person with the highest years of programming experience during the job interview using any rust compound data type

--- week-8/project_2/src/main.rs
+++ week-8/project_2/src/main.rs
@@ -1,3 +1,30 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut candidates = Vec::new();
+
+    loop {
+        let mut name = String::new();
+        println!("Enter candidate's name (or 'quit' to finish):");
+        io::stdin().read_line(&mut name).expect("Error reading input");
+        let name = name.trim();
+
+        if name == "quit" {
+            break;
+        }
+
+        let mut experience_str = String::new();
+        println!("Enter years of experience:");
+        io::stdin().read_line(&mut experience_str).expect("Error reading input");
+        let experience: u32 = experience_str.trim().parse().expect("Please enter a valid number");
+
+        candidates.push((name.to_string(), experience));
+    }
+
+    println!("\nCandidates with more than 5 years of experience:");
+    for (name, experience) in candidates {
+        if experience > 5 {
+            println!("{}, {} years", name, experience);
+        }
+    }
 }
