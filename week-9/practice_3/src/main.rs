0ca7dd570b61bc041bd2a14b19d04617325219fa
00danem00 <daniel.emezi@pau.edu.ng>
2025-11-26 13:12:14 +0100

    i used the remove_file() function to delete a file.

--- week-9/practice_3/src/main.rs
+++ week-9/practice_3/src/main.rs
@@ -3,6 +3,7 @@ use std::fs;
 
 
 fn main() {
+    let file = std::fs::File::create("data.txt").expect("create failed");
     fs::remove_file("data.txt").expect("could not remove file");
     println!("file is removed");
 }
