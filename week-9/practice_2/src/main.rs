f25d8ffc013e9a4ee1b9019e6350c342758b5c98
00danem00 <daniel.emezi@pau.edu.ng>
2025-11-26 13:10:51 +0100

    i read to a file using 'std::io::Read' module

--- week-9/practice_2/src/main.rs
+++ week-9/practice_2/src/main.rs
@@ -2,6 +2,7 @@ use std::io::Read;
 
 
 fn main() {
+    let mut file = std::fs::File::create("welcome_message.txt").expect("create failed");
     let mut file = std::fs::File::open("welcome_message.txt").unwrap();
     let mut contents = String::new();
     
